68632cc84e558b805583975409895d942bf87bf4
Hana Guta <hana.guta-ug@aau.edu.et>
2024-08-19 07:10:37 +0300

    add sidebar and header in to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "../components/sidebar";  // Sidebar component
-import Header from "../components/header";    // Header component
+import Sidebar from "../components/sidebar";  
+import Header from "../components/header";    
 
 const inter = Inter({ subsets: ["latin"] });
 
