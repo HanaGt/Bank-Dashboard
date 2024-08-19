079c25ed94ab2258e085a1b2aca4938f904ff29b
Hana Guta <hana.guta-ug@aau.edu.et>
2024-08-19 17:50:08 +0300

    style the header

--- components/header.tsx
+++ components/header.tsx
@@ -12,21 +12,28 @@ const Header: React.FC = () => {
           <input
             type="text"
             placeholder="Search for something"
-            className="w-64 text-searchColor py-2 pl-10 pr-4 bg-gray-100 rounded-3xl focus:outline-none"
+            className="w-64 text-blue-200 py-3 pl-10 pr-4 bg-gray-100 rounded-3xl focus:outline-none"
           />
-          <span className="absolute left-3 top-2.5 text-gray-500 ">
+          <span className="absolute left-3 top-4 text-blue-200 ">
             <FiSearch />
           </span>
         </div>
-        <FiSettings className="text-gray-600 bg-customGray w-6 h-6 cursor-pointer" />
+        <div className="bg-gray-200 w-12 h-12 rounded-full flex items-center justify-center cursor-pointer">
+            <FiSettings className="text-blue-200 w-6 h-6" />
+        </div>
+
+
 
         <div className="relative">
-          <FiBell className="text-gray-600 w-6 h-6 cursor-pointer" />
-          <span className="absolute top-0 right-0 inline-flex items-center justify-center w-2 h-2 p-2 text-xs font-bold text-white bg-red-500 rounded-full">
+        <div className="bg-gray-200 w-12 h-12 rounded-full flex items-center justify-center cursor-pointer">
+            <FiBell className="text-blue-200 w-6 h-6" />
+            <span className="absolute top-0 right-0 inline-flex items-center justify-center w-2 h-2 p-2 text-xs font-bold text-white bg-red-500 rounded-full">
             3
           </span>
         </div>
-        <div className="w-10 h-10">
+          
+        </div>
+        <div className="w-12 h-12">
           <Image
             width={60}
             height={60}
